b858c6791b6e97b933d04a66886605c9aca3de34
John D. Lewis <balinjdl@gmail.com>
2011-07-17 21:45:03 -0400

    Added hit handling (via containsPoint(...)), added links (new Link class + new ArrayList<Link>) with display; added highlighting, and screen capture (to PDF); simplified OTNT.java (moving functionality to utility class or Bible.java)

--- src/com/balinsbooks/otnt/Book.java
+++ src/com/balinsbooks/otnt/Book.java
@@ -1,8 +1,9 @@
 package com.balinsbooks.otnt;
 
-import java.awt.Point;
-
 import processing.core.PApplet;
+
+import com.balinsbooks.otnt.utils.FloatPoint;
+import com.balinsbooks.otnt.utils.PointMath;
 
 public class Book {
 	String bookName;
@@ -11,10 +12,41 @@ public class Book {
 	public int R;
 	public int G;
 	public int B;
+	
+	public int strokeWeight = 1;
+
+	float radiusInner;
+	FloatPoint startInner, startOuter, endInner, endOuter;
+	double startR, endR, arcThickness;
 
 	Book(String name, String testament, int chpCount) {
 		bookName = name;
 		bookTestament = testament;
 		numChapters = chpCount;
+
+		startInner = new FloatPoint();
+		endInner = new FloatPoint();
+		startOuter = new FloatPoint();
+		endOuter = new FloatPoint();
+	}
+
+	public boolean containsPoint(float x, float y, float sngCenterX, float sngCenterY) {
+		// sngAngle = in degrees
+		double sngAngle = PointMath.calcAngle(x, y, sngCenterX, sngCenterY);
+
+		double distanceX = Math.abs(x - sngCenterX);
+		double distanceY = Math.abs(y - sngCenterY);
+		double distanceFromCenter = Math.sqrt((distanceX * distanceX) + (distanceY * distanceY));
+
+//		System.err.println(bookName + "; sngAngle: " + sngAngle + "; startRdeg: " + Math.toDegrees(startR) + "; endRdeg: " + Math.toDegrees(endR));
+		
+		//&& (sngAngle >= Math.toDegrees(startR) && sngAngle <= Math.toDegrees(endR))
+		if ((distanceFromCenter >= (radiusInner / 2) && distanceFromCenter <= ((radiusInner / 2) + (arcThickness / 2)) 
+				&& (sngAngle >= Math.toDegrees(startR) && sngAngle <= Math.toDegrees(endR)))) {
+//			System.err.println("containsPoint: yes! " + bookName);
+			return (true);
+		} else {
+			return (false);
+		}
 	}
 }
--- src/com/balinsbooks/otnt/OTNT.java
+++ src/com/balinsbooks/otnt/OTNT.java
@@ -1,9 +1,10 @@
 package com.balinsbooks.otnt;
 
-import java.awt.Point;
 import java.util.Iterator;
 
 import processing.core.PApplet;
+import processing.core.PFont;
+import processing.pdf.*;
 
 public class OTNT extends PApplet {
 	private static final long serialVersionUID = 8005850003613011975L;
@@ -14,25 +15,44 @@ public class OTNT extends PApplet {
 	Iterator<Book> iter;
 
 	float scaler = 1;
-
 	public int cx, cy;
-	
+	int bkId = -1;
+
+	PFont font;
+
+	boolean record = false;
+
+	private boolean highlight = false;
+	private boolean freeze = false;
+
 	public void setup() {
+		// font = createFont("Calibri", 24, true);
+		// textFont(font,36);
+
 		debug = true;
 		size(1000, 1000);
 		background(255);
 		smooth();
 
 		books = new Bible(this);
-//		debugPrint("chpAngleR = " + chpAngleR + " (deg: " + chpAngleD + "); bookBuffer = " + bookBufferR
-//				+ " (deg: " + bookBufferD + ")");
+		// debugPrint("chpAngleR = " + chpAngleR + " (deg: " + chpAngleD +
+		// "); bookBuffer = " + bookBufferR
+		// + " (deg: " + bookBufferD + ")");
 
 		books.draw();
-		
+
+		// String[] fontList = PFont.list();
+		// println(fontList);
+
 		debug = false;
 	}
 
 	public void draw() {
+		if (record) {
+			// Note that #### will be replaced with the frame number. Fancy!
+			beginRecord(PDF, "frame-####.pdf");
+		}
+
 		background(255);
 
 		translate(width / 2, height / 2);
@@ -40,10 +60,36 @@ public class OTNT extends PApplet {
 		translate(-width / 2, -height / 2);
 
 		books.draw();
-		
+
+		if (record) {
+			endRecord();
+			record = false;
+		}
+
+		if (!freeze) {
+			if (highlight && (bkId = books.inBook(mouseX, mouseY)) >= 0) {
+				books.highlight(bkId);
+			} else {
+				books.unhighlight();
+			}
+		}
+
+		fill(126);
+		textAlign(LEFT);
+//		text("mouse loc: " + mouseX + ", " + mouseY + "; inbook? " + books.getName(books.inBook(mouseX, mouseY)), 0, 30);
+		int id = books.inBook(mouseX, mouseY);
+		String name =books.getName(id); 
+		if (name.length() > 0) {
+			text(name, 10, 30);
+			text("links in/out: " + String.valueOf(books.getLinkCount(id)), 10, 60);
+		}		
 	}
 
 	public void keyPressed() {
+		if (keyCode == PApplet.SHIFT) { // highlight on
+			highlight = false;
+		}
+
 		if (key == '-') {
 			scaler -= 0.1;
 		}
@@ -66,6 +112,16 @@ public class OTNT extends PApplet {
 			scaler = 1;
 			books.cx = width / 2;
 			books.cy = height / 2;
+		}
+		if (key == 'f') {
+			record = true;
+			freeze = !freeze;
+		}
+	}
+
+	public void keyReleased() {
+		if (keyCode == PApplet.SHIFT) {
+			highlight = true;
 		}
 	}
 }
